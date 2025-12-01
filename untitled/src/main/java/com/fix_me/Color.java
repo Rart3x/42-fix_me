9291b6ee0175c0e81881814f55fb5e517d8fab0d
Rart3x <ramjattankeny971@gmail.com>
2025-12-01 16:41:03 +0100

    feat: create socket Server class

---
+++ untitled/src/main/java/com/fix_me/Color.java
@@ -0,0 +1,8 @@
+package com.fix_me;
+
+public class Color {
+    public static final String RESET = "\u001B[0m";
+    public static final String GREEN = "\u001B[32m";
+    public static final String BLUE = "\u001B[34m";
+    public static final String RED = "\u001B[31m";
+}
