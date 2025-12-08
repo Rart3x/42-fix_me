be424135fe93b718d87f77009064315aafcdc15b
Rart3x <ramjattankeny971@gmail.com>
2025-12-08 23:11:50 +0100

    feat: add Router class, listening on 2 ports

--- src/main/java/kramjatt/fix_me/Main.java
+++ src/main/java/kramjatt/fix_me/Main.java
@@ -4,8 +4,11 @@ public class Main  {
     public static void main(String[] args) {
         Broker broker = new Broker();
         Market market = new Market();
+        Router router = new Router();
 
-        broker.start();
-        market.start();
+        //broker.start();
+        //market.start();
+
+        router.start();
     }
 }
